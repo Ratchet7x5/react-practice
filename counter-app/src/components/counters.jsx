ab05b56d3f0ba440d9d3ac8bb19274e449051da3
Tarun Ramachandran <36789694+Ratchet7x5@users.noreply.github.com>
2022-06-23 22:40:16 +1200

    add new logic (counters + counter)

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,16 +4,22 @@ import Counter from './counter';
 class Counters extends Component {
     state = { 
         counters: [
-            {id: 0, counter: 0},
-            {id: 1, counter: 0},
-            {id: 2, counter: 0},
-            {id: 3, counter: 0},
+            {id: 1, value: 0},
+            {id: 2, value: 0},
+            {id: 3, value: 0},
+            {id: 4, value: 0},
         ]
      } 
 
+     handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        counters[0].value++;
+     }
+
      handleDelete = counterId =>
      {
-        console.log('Event Handler Called: ', counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
      }
 
     render() {
@@ -22,7 +28,11 @@ class Counters extends Component {
         <div>
             { this.state.counters.map(counter => 
                 (
-                    <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}/>
+                    <Counter 
+                    key={counter.id} 
+                    onDelete={this.handleDelete} 
+                    onIncrement={this.handleIncrement}
+                    counter={counter}/>
                 )) 
             }
         </div>); 
