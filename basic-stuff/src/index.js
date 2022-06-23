a1cfb4b3c34a51d7e32473c45bd01a1d998410bf
Tarun Ramachandran <36789694+Ratchet7x5@users.noreply.github.com>
2022-06-23 22:45:57 +1200

    update greeting component

--- basic-stuff/src/index.js
+++ basic-stuff/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 function Greeting()
 {
-  return <p>This is my first component, I think...</p>
+  return <p>This is my first component, I think...</p>;
 }
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'));
